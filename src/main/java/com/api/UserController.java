8d97cc6030210978a41b717600e9d2a315a3b681
education-bootcamp <education.seekerscloud@gmail.com>
2025-08-16 10:34:17 +0530

    init

--- src/main/java/com/api/UserController.java
+++ src/main/java/com/api/UserController.java
@@ -1,9 +1,19 @@
 package com.api;
 
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cpd.hotel_system.auth_service_api.config.JwtService;
+import com.cpd.hotel_system.auth_service_api.dto.request.SystemUserRequestDto;
 import com.cpd.hotel_system.auth_service_api.service.SystemUserService;
+import com.cpd.hotel_system.auth_service_api.util.StandardResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,4 +23,16 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     private final SystemUserService systemUserService;
     private final JwtService jwtService;
+
+    @PostMapping("/visitors/signup")
+    public ResponseEntity<StandardResponseDto> createUser(
+        @RequestBody SystemUserRequestDto dto
+    ) throws IOException{
+        systemUserService.createUser(dto);
+        return new ResponseEntity<>(
+            new StandardResponseDto(201,"user account was created",null),
+            HttpStatus.CREATED
+        );
+    }
+
 }
